b7072e3d1ffa20661ddf82cd5909ff54582d4cc3
Diego Ferreira dos Santos <santos.diegoferreira@gmail.com>
2026-05-14 00:31:07 -0300

    feat: ingest

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,29 @@
-from search import search_prompt
+from search import search_prompt, PROMPT_TEMPLATE
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-5-nano", temperature=0.5)
+system = ("system", PROMPT_TEMPLATE)
+chat_prompt = ChatPromptTemplate([system])
+
+pergunta = "Qual o faturamento da empresa X?"
 
 def main():
-    chain = search_prompt()
+    chain = search_prompt(pergunta)
 
     if not chain:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
     
-    pass
+    messages = chat_prompt.format(
+        pergunta=pergunta,
+        contexto=chain
+    )
+    result = model.invoke(messages)
+    print(result.content)
+    
 
 if __name__ == "__main__":
     main()
--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,28 @@
 import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_postgres.vectorstores import PGVector
+from langchain_openai import OpenAIEmbeddings
 
 load_dotenv()
 
 PDF_PATH = os.getenv("PDF_PATH")
+file_path = "./relatorio.pdf";
+loader = PyPDFLoader(file_path)
+text_spliter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 
-def ingest_pdf():
-    pass
+vector_store = PGVector(
+    embeddings=OpenAIEmbeddings(model="text-embedding-3-small"),
+    collection_name="documentos",
+    connection="postgresql+psycopg://postgres:postgres@localhost:5432/rag",
+    use_jsonb=True
+)
 
+def ingest_pdf():    
+    docs = loader.load()
+    all_splits = text_spliter.split_documents(docs)
+    vector_store.add_documents(documents=all_splits)
 
 if __name__ == "__main__":
     ingest_pdf()
