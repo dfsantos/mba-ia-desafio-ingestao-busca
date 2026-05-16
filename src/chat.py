c7901409a036c05923332a54bf1cf6450c7773f9
Diego Ferreira dos Santos <santos.diegoferreira@gmail.com>
2026-05-16 19:48:45 -0300

    corrige bug de imports e nome do PDF

--- src/chat.py
+++ src/chat.py
@@ -1,8 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from search import search_prompt, PROMPT_TEMPLATE
 from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
-load_dotenv()
 
 model = ChatOpenAI(model="gpt-5-nano", temperature=0.5)
 system = ("system", PROMPT_TEMPLATE)
--- src/ingest.py
+++ src/ingest.py
@@ -1,14 +1,14 @@
-import os
 from dotenv import load_dotenv
+load_dotenv()
+
+import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_postgres.vectorstores import PGVector
 from langchain_openai import OpenAIEmbeddings
 
-load_dotenv()
-
 PDF_PATH = os.getenv("PDF_PATH")
-file_path = "./relatorio.pdf";
+file_path = "./document.pdf";
 loader = PyPDFLoader(file_path)
 text_spliter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 
